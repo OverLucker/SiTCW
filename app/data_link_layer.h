207356b7bf6d6cb779f2526fe033122a0edcfd30
OverLucker <lucker1005000@gmail.com>
2017-04-14 14:40:45 +0300

    Добавлен классы Frame и FrameBuilder(описание кадра и методы для их построения)

--- app/data_link_layer.h
+++ app/data_link_layer.h
@@ -16,10 +16,10 @@
 
 
 enum class SuperVisorFrameTypes {
-	Meeting,
-	Disconnect,
-	Login,
-	Logout
+	Meeting = 1,
+	Disconnect = 2,
+	Login = 3,
+	Logout = 4
 };
 
 
@@ -30,229 +30,144 @@ enum class SuperVisorFrameTypes {
 #define FRAME_SECURE_BYTE 0x7E
 
 enum class FrameType {
+	Null = 0,
 	SuperVisor = 1,
 	Information = 2,
 	Token = 3,
 };
 
-struct Frame {
+class Frame {
+private:
 	FrameType frame_type;
-	bool last_frame;
+	quint8 last_frame;
 	quint8 frame_header_size;
 	quint8 frame_secure;
 	quint8 sender;
 	quint8 recipients_number;
-	quint8 recipients[14];
+	QVector<quint8> recipients;
 	quint8 data_size;
-	char data[MAX_DATA_SIZE];
-};
+	QByteArray data;
 
-namespace Handlers {
-	class FrameHandler {
-		public:
-			virtual bool handle(const Frame& frame) = 0;
-
-		operator QByteArray()const {
-
-		}
-	};
-
-	class SuperVisorHandler {
-
-	};
-
-	class InformationHandler {
-
-	};
-
-	class TokenHandler {
-
-	};
-}
-
-
-
-
-
-
-
-
-/*
-typedef unsigned char uchar; 
-
-const int TOKEN_MAX_TIMEOUT = 15;
-
-
-
-
-
-
-
-class Frame {
-	// This class is immutable. It just contains gathered info
-
-private:
-	
-
-	
-
-	// validators
-	bool validate_frame_type(quint8 val){ return val > 0 && val < 4; }
+	const int address_size = 4; // in bits
+	const int pure_header_size = 4; // in bytes
 
 public:
-	Frame(
-		FrameType frame_type,
-		bool last_frame,
-		quint8 frame_header_size,
-		bool frame_secure,
-		quint8 sender,
-		std::vector<quint8> recipients,
-		quint8 data_size
-		// ToDo: need to add data
-	) {
+
+	// ToDo: forgot about frame number
+	Frame(FrameType frame_type,
+		  bool last_frame,
+		  const quint8& sender, 
+		  const QVector<quint8>& recipients,
+		  const QByteArray& data) {
+
 		this->frame_type = frame_type;
-		this->last_frame = last_frame;
-		this->frame_header_size = frame_header_size;
-		this->frame_secure = frame_secure;
+		this->last_frame = last_frame ? 0xFF : 0x00;
+
+		this->frame_secure = FRAME_SECURE_BYTE;
+
 		this->sender = sender;
+		this->recipients_number = recipients.length();
+
 		this->recipients = recipients;
-		this->data_size = data_size;
-		// ToDo: need to add data
-	}
-	
-	static const std::vector<Frame>& Construct(quint8 * data, int length) {
-	
+		this->data_size = data.length();
+		this->data = data;
+
+		this->frame_header_size = (recipients_number*address_size%8 ? 1 : 0) + (recipients_number * address_size / 8) + pure_header_size;
 	}
 
-	static const std::vector<Frame>& Construct(QDataStream& data) {
-		// one Frame Reading
-		quint8 tmp;
+	operator QByteArray() {
+		QByteArray buffer;
 
-		// getting 1 byte
-		data >> tmp;
-		bool last_frame = tmp & 0x20;
-		quint8 frame_type = tmp && 0xC0;
-		quint8 frame_header_size = tmp && 0x1F;
-		
-		// getting 2 byte
-		data >> tmp;
-		quint8 frame_number = tmp;
+		quint8 f = (quint8)frame_type << 6;
+		quint8 byte = f | (last_frame & 0x20) | (frame_header_size & 0x1F);
+		buffer.append(byte);
 
-		// getting 3 byte
-		data >> tmp;
-		bool frame_secure = tmp == 0xBD;
-		if (!frame_secure)
-			// return an empty vector
-			return std::vector<Frame>();
+		buffer.append(frame_secure);
 
-		// getting 4 byte
-		data >> tmp;
-		quint8 sender = tmp & 0xF0;
-		quint8 recipient_number = tmp & 0x0F;
-		quint8 recipient_bytes = recipient_number >> 1;
-
-		// checking
-		// static bytes count = 5
-		if (frame_header_size != recipient_bytes + 5)
-			// return an empty vector
-			return std::vector<Frame>();
-
-		// getting recipients
-		std::vector<quint8> recipients;
-		
-		for (int i = 0; i < recipient_bytes; ++i)
-		{
-			data >> tmp;
-			if (tmp & 0xF0)
-				recipients.push_back(tmp & 0xF0);
-
-			if (tmp & 0x0F)
-				recipients.push_back(tmp & 0x0F);
+		buffer.append((sender << 4) | (recipients_number & 0x0F));
+		for (quint8 i = 0; i < recipients_number; i += 2) {
+			if (i + 1 < recipients_number)
+				buffer.append((recipients[i] << 4) | (recipients[i + 1] & 0x0F));
+			else
+				buffer.append(recipients[i] << 4);
 		}
 
-		// getting data_size
-		quint8 data_size;
-		data >> data_size;
+		buffer.append(data_size);
 
-		// ToDo: need to read data
-		char d[256];
-		data.readRawData(d, data_size);
-
+		buffer.append(data);
+		return buffer;
 	}
 
-
-	// getters
 	FrameType getFrameType() { return this->frame_type; }
 	bool isLastFrame() { return this->last_frame; }
 	quint8 const getFrameHeaderSize() { return this->frame_header_size; }
 	quint8 const getSender() { return this->sender; }
-	std::vector<quint8> const getRecipients() { return this->recipients; }
-	quint8 const getDataSize() { return this->data_size; }
-	char * getData() { return this->data; }
-
+	QVector<quint8> const getRecipients() { return this->recipients; }
+	QByteArray getData() { return this->data; }
 };
 
 
+class FrameBuilder {
 
+public:
+	static Frame makeInformationFrame(const quint8& sender, const QVector<quint8>& recipients, const QByteArray& data) {
+		// ToDo: split to frames
+		return Frame (
+			FrameType::Information,
+			true,
+			sender,
+			recipients,
+			data
+		);
+	}
 
+	static Frame makeSupervisorFrame(const quint8& sender, const QByteArray& data) {
+		// ToDo: split to frames
+		QVector<quint8> recipients;
+		for (int i = 1; i < 16; ++i) {
+			recipients.append(i);
+		}
 
+		return Frame(
+			FrameType::SuperVisor,
+			true,
+			sender,
+			recipients,
+			data
+		);
+	}
 
+	static Frame makeNullFrame() {
+		return Frame(
+			FrameType::Null,
+			true,
+			0,
+			QVector<quint8>(),
+			QByteArray()
+		);
+	}
 
-//QMap<FrameType, handler> handlers;
+	static Frame fromByteArray(const QByteArray& data) {
+		if (data.length() <= FRAME_HEADER_SIZE)
+			return makeNullFrame();
 
-void supervisor_handler(char * data) {
+		if (data[1] != FRAME_SECURE_BYTE)
+			return makeNullFrame();
 
-}
+		QVector<quint8> recipients;
+		for (quint8 i = 0, all = (data[2] & 0x0F) / 2; i < all; ++i) {
+			if ((data[i+3] & 0xF0 >> 4) > 0)
+				recipients.append((data[i + 3] & 0xF0) >> 4);
+			if((data[i+3] & 0x0F) > 0)
+				recipients.append((data[i + 3] & 0x0F));
+		}
+		return Frame(
+			FrameType(data[0] >> 6), // frame_type
+			data[0] & 0x20, // last_frame
+			(data[2] & 0xF0) >> 4, // sender
+			recipients,
+			data.mid(data[0] & 0x1F)
+		);
+	}
 
-void information_handler(char * data) {
-
-}
-
-void token_handler(char * data) {
-
-}
-
-void ringdestruction_handler(char * data) {
-
-}
-	
-
-
-
-
-
-
-
-
-
-
-
-char * frame_maker(char * data) {
-	return NULL;
-}
-
-void dl_api_send() {
-
-}
-
-
-
-int frame_checker(char * data) {
-	// extract frame info (type, last frame, size of frame)
-
-	// extract frame number
-
-	// extract sender
-	// extract recipient_count
-	// extract recipients
-	// extract user_data_count
-	// extract user_data
-	return NULL;
-}
-
-// dl_api_send
-// dl_api_get(void * callback)
-
-*/
-
-#endif // !DATA_LINK_LAYER
+};
