1b43bb62c189ec5e5572dcc7c9f3093f54d46d48
OverLucker <lucker1005000@gmail.com>
2017-05-03 21:36:58 +0300

    Добавление виджета для создания обработчиков

---
+++ app/messagewidget.cpp
@@ -0,0 +1,42 @@
+#include "messagewidget.h"
+#include "ui_messagewidget.h"
+
+// VERY IMPORTANT THING!!!
+#pragma execution_character_set("utf-8")
+
+
+MessageWidget::MessageWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MessageWidget)
+{
+    ui->setupUi(this);
+	QPalette p(this->palette());
+	p.setColor(QPalette::Background, Qt::red);
+	this->setPalette(p);
+}
+
+MessageWidget::~MessageWidget()
+{
+    delete ui;
+}
+
+void MessageWidget::display_message(Message message) {
+	this->message = message;
+	this->setBackgroundRole(QPalette::ColorRole::Base);
+	ui->lbMessage->setText(message.getMessage());
+	ui->lbMessage->hide();
+	ui->lbFROM->setText(QString("%1: %2").arg("FROM", message.getSender()));
+	ui->lbSTATUS->setText(QString("%1: %2").arg("STATUS", "Не прочитано"));
+	ui->lbTO->setText(QString("%1: %2").arg("TO", message.getRecepient()));
+}
+
+void MessageWidget::mousePressEvent(QMouseEvent* ev) {
+	ui->lbMessage->show();
+	it->setSizeHint(this->sizeHint());
+	ui->lbSTATUS->setText(QString("%1: %2").arg("STATUS", "Прочитано"));
+	emit messageRead(this->message);
+}
+
+void MessageWidget::setItem(QListWidgetItem * it) {
+	this->it = it;
+}
---
+++ app/messagewidget.h
@@ -0,0 +1,34 @@
+#ifndef MESSAGEWIDGET_H
+#define MESSAGEWIDGET_H
+
+#include <QWidget>
+#include <qlistwidget.h>
+#include "message.h"
+
+namespace Ui {
+class MessageWidget;
+}
+
+class MessageWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MessageWidget(QWidget *parent = 0);
+	void display_message(Message);
+	void setItem(QListWidgetItem *);
+    ~MessageWidget();
+
+private:
+    Ui::MessageWidget *ui;
+	Message message;
+	QListWidgetItem * it;
+
+protected:
+	void mousePressEvent(QMouseEvent*);
+
+signals:
+	void messageRead(Message);
+};
+
+#endif // MESSAGEWIDGET_H
